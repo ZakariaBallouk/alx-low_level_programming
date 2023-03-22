2259358b9a10afba6881c7817e7f1a0b9bc2125e
ZakariaBallouk <tpmazem@gmail.com>
2023-03-22 15:08:02 +0000

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,20 @@
+
+
 #ifndef MAIN_H
 #define MAIN_H
-int putchar(char c);
-void print_alphabet (void);
+
+int _putchar(char c);
+void print_alphabet(void);
 void print_alphabet_x10(void);
-int islower(int c);
-int isalpha(int c);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
-int abs(int);
+int _abs(int);
 int print_last_digit(int);
-void jack_bauer(void); 
+void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
 #endif
