4389997df06675cdda1aee64c0187f59937b521e
ZakariaBallouk <tpmazem@gmail.com>
2023-03-23 12:08:59 +0000

    README file addition

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MY_ISUPPER
+#define MY_ISUPPER
+#include <ctype.h>
+int _isupper(int c);
+{
+  char c ;
+    if (isupper(c)) {
+        int r=1;
+    } else {
+        r=0;
+    }
+return r;
+#endif
